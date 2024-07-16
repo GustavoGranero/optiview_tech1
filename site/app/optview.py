c028384b879a74c771a9d34247248269c2b042b1
GustavoGranero <ghtgranero@gmail.com>
2024-07-15 23:08:39 -0300

    Putting sidebar and footbar in includes, sidebar and slides javascript in js include, removin templates and static folders all templestess  will be in web folder.

--- site/app/optview.py
+++ site/app/optview.py
@@ -2,26 +2,17 @@ from flask import Flask
 from flask import render_template
 import datetime
 
-app = Flask(__name__, static_url_path='',  static_folder='../web',)
+app = Flask(__name__, static_url_path='',  static_folder='../web', template_folder="../web")
 
 def authentication_test():
-        # verificar aqui se está autenticado ...
+    # verificar aqui se está autenticado ...
     #  ... se estiver vai para a página
     # ... se não estiver vai para o login
     # chamar isso em todas as páginas
     pass
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
-
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello(name = None):
-    print("aaaaa")
-    return render_template("hello_there.html", name=name, date=datetime.datetime.now())
-
-# @app.route("/login.html")
-# def login():
-#     return app.send_static_file("login.html")
+@app.route("/<name>")
+def templates(name = "home.html"):
+    return render_template(name)
 
