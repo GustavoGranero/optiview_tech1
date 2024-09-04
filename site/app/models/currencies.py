7027b2e6915858aa47134227ced846ab9e661b61
Airton da Fonseca Granero <agranero@gmail.com>
2024-09-04 18:59:17 -0300

    Mais uma mudança para refletir a mudança do nome da tabela currency para currencies que escapou.

--- site/app/models/currencies.py
+++ site/app/models/currencies.py
@@ -3,7 +3,7 @@ from optview import db
 
 
 class Currencies(db.Model):
-    __tablename__ = "currency"
+    __tablename__ = "currencies"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
